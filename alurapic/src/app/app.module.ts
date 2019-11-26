bdaa2c9deb6d0a4098d6501475d75eeffd4f6e0d
Thiago de Moura Parracho <TMPAR@bndes.net>
2019-11-26 16:13:29 -0300

    novo

---
+++ alurapic/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { PhotoComponent } from './photo/photo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhotoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
