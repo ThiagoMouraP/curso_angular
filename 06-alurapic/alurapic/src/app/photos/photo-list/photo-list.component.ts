53b6ca7381aa837bb39a0a36225a53d69bcd3f6b
Thiago de Moura Parracho <TMPAR@bndes.net>
2019-12-16 18:40:14 -0300

    final da parte 6 e início da parte 7

--- 06-alurapic/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ 06-alurapic/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
-
+export class PhotoListComponent implements OnInit{
+  
   photos: Photo[] = []; 
   filter: string = '';
-
-  constructor(private activatedRoute: ActivatedRoute) { }
-
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService  
+  ) { }
+  
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
+  }
+
+  load(){
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage )
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if(!photos.length) this.hasMore = false;
+      })
   }
 }
